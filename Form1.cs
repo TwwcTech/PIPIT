bfdb23614820ae16d7ddc1cf439bc4684bbdef19
twwct <twwc.tech@gmail.com>
2023-09-17 15:58:39 -0600

    todo reformatted

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,7 @@ namespace PIPIT
         private void MainForm_Load(object sender, EventArgs e)
         {
             /* + Add a "NotifyIcon" control to add icon to task bar.
+             * 
              * + Add a "ContextMenuStrip" to the "NoitfyIcon", this 
              *   will allow us to diplay menu items when the user 
              *   right-clicks the taskbar icon. */
