73fd0937b4408b1fc157dad42034e32d8c96f9f6
twwct <twwc.tech@gmail.com>
2023-09-17 17:10:15 -0600

    removed uneccessary code

--- Backend/WinRegistry/RegiMan.cs
+++ Backend/WinRegistry/RegiMan.cs
@@ -7,22 +7,21 @@ namespace PIPIT.Backend.WinRegistry
     {
         public static void RegisterApp(string appName, string appVersion)
         {
-            /* + Create a folder with the apps name and hold regkey values
-             *   that show when app was firt launched.
+            /* + Create a folder with the apps name and hold regkey values that 
+             *   show when app was firt launched in "Software\\" folder
              *       - Regkey value: InitDate (DateTime) - Timestamp of when app was first launched
              *       - Regkey value: StartupEnabled (bool) - True or False if the tray app launches during startup (editable) */
         }
 
         public static void AddToStartup(string appName)
         {
-            using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true)!)
+            using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true)!) // Use "SOFTWARE" if having errors
             {
                 if (regKey != null)
                 {
                     try
                     {
-                        string targetExe = Assembly.GetExecutingAssembly().Location;
-                        regKey.SetValue(appName, targetExe);
+                        regKey.SetValue(appName, Assembly.GetExecutingAssembly().Location);
                     }
                     catch (Exception ex) { throw new Exception(ex.ToString()); }
                 }
