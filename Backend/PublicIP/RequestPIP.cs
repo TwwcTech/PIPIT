91efea93532a4eda8852e38773d89b03f5d6568a
twwct <twwc.tech@gmail.com>
2023-09-17 15:35:02 -0600

    static method created

--- Backend/PublicIP/RequestPIP.cs
+++ Backend/PublicIP/RequestPIP.cs
@@ -1,6 +1,25 @@
-﻿namespace PIPIT.Backend.PublicIP
+﻿using Newtonsoft.Json;
+using PIPIT.Backend.PublicIP.PIPResources;
+
+namespace PIPIT.Backend.PublicIP
 {
     internal class RequestPIP
     {
+        public static async Task<PIPSetters> RequestJson()
+        {
+            PIPSetters jsonSetters;
+            using (HttpClient client = new())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+                    string jsonContent = await client.GetStringAsync(@"https://ifconfig.co/json");
+
+                    jsonSetters = JsonConvert.DeserializeObject<PIPSetters>(jsonContent)!;
+                    return jsonSetters;
+                }
+                catch (HttpRequestException ex) { throw new HttpRequestException(ex.ToString()); }
+            }
+        }
     }
 }
