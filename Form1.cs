d69a8db444c0bc8ff7819f45ee49a003dac1b364
twwct <twwc.tech@gmail.com>
2023-09-17 22:21:10 -0600

    on form close handler method created

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using PIPIT.Backend.WinRegistry;
+
 namespace PIPIT
 {
     public partial class MainForm : Form
@@ -14,7 +16,21 @@ namespace PIPIT
              *   - if it has, check if the "IsStartupEnabled" option
              *     is "true" or "false":
              *     > true = skip the window and only start the trayicon app
-             *     > false = open the setup window on startup */
+             *     > false = open the options window on startup */
+            bool registeredStatus = RegiMan.IsAppRegistered();
+            if (registeredStatus)
+            {
+                bool isStartupEnabled = RegiMan.IsStartupEnabled();
+                if (isStartupEnabled)
+                {
+                    Hide();
+                    TrayIcon.Visible = true;
+                }
+                else
+                {
+
+                }
+            }
         }
 
         private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -30,6 +46,13 @@ namespace PIPIT
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Exit menu item
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            WindowState = FormWindowState.Minimized;
+            Hide();
         }
     }
 }
