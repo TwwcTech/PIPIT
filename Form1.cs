3d438b349cf1f26f27a77050aa5fe56c3d49dcb7
twwct <twwc.tech@gmail.com>
2023-09-17 21:53:21 -0600

    method updates

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,27 @@ namespace PIPIT
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            /* + Check if the app has ran before (registerd):
+             *   - if not prompt the user with the setup window
+             *   - if it has, check if the "DontShowSetup" option
+             *     is "true" or "false":
+             *     > true = skip the window and only start the trayicon app
+             *     > false = open the setup window on startup */
+        }
 
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowDialog(this);
+        }
+
+        private void ifconfigcoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // ifconfig.co menu item
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit menu item
         }
     }
 }
