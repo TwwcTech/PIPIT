5b6ba87c75e39f6bd69d7aa90f367f7d37a8e8cf
twwct <twwc.tech@gmail.com>
2023-09-17 18:43:15 -0600

    ShortcutPath property created

--- AppResources/StaticResources.cs
+++ AppResources/StaticResources.cs
@@ -1,6 +1,23 @@
-﻿namespace PIPIT.AppResources
+﻿using System.Reflection;
+
+
+namespace PIPIT.AppResources
 {
     internal class StaticResources
     {
+        public const string AppName = "";
+        public const string Version = "";
+
+        public static string DateTimeStamp
+        {
+            get { return DateTimeStamp; }
+            private set { DateTimeStamp = DateTime.Now.ToString("ddMMyyyy"); }
+        }
+
+        public static string ShortcutPath
+        {
+            get { return ShortcutPath; }
+            private set { ShortcutPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, $"{AppName}.lnk"); }
+        }
     }
 }
