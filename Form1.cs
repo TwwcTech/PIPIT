506b0d3e931b695fea934315ca086051eb94647b
twwct <twwc.tech@gmail.com>
2023-09-17 22:43:44 -0600

    todo removed

--- Form1.cs
+++ Form1.cs
@@ -11,12 +11,6 @@ namespace PIPIT
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            /* + Check if the app has ran before (registerd):
-             *   - if not prompt the user with the setup window
-             *   - if it has, check if the "IsStartupEnabled" option
-             *     is "true" or "false":
-             *     > true = skip the window and only start the trayicon app
-             *     > false = open the options window on startup */
             if (RegiMan.IsAppRegistered())
             {
                 if (RegiMan.IsStartupEnabled())
