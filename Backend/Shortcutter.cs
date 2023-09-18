5c1b27f8ebe53decd60e879b674103a8fb0fe501
twwct <twwc.tech@gmail.com>
2023-09-17 18:43:47 -0600

    class and static method created to create shortcuts

---
+++ Backend/Shortcutter.cs
@@ -0,0 +1,23 @@
+﻿using PIPIT.AppResources;
+using System.Reflection;
+using System.Security.Policy;
+
+namespace PIPIT.Backend
+{
+    internal class Shortcutter
+    {
+        public static void CreateShortcut()
+        {
+            using (StreamWriter shortcutWriter = new StreamWriter(StaticResources.ShortcutPath))
+            {
+                shortcutWriter.WriteLine("[Desktop Entry]");
+                shortcutWriter.WriteLine($"Version={StaticResources.Version}");
+                shortcutWriter.WriteLine($"Name={Path.GetFileNameWithoutExtension(StaticResources.ShortcutPath)}");
+                shortcutWriter.WriteLine($"Exec={Assembly.GetExecutingAssembly().Location}");
+                shortcutWriter.WriteLine("Terminal=false");
+                shortcutWriter.WriteLine("Type=Application");
+                shortcutWriter.Flush();
+            }
+        }
+    }
+}
