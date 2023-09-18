0c27c16d7c89c694a6dade31c3e48533fc106419
twwct <twwc.tech@gmail.com>
2023-09-17 21:33:44 -0600

    UI updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            TrayIcon = new NotifyIcon(components);
+            TrayIconMenu = new ContextMenuStrip(components);
+            opToolStripMenuItem = new ToolStripMenuItem();
+            webToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            TrayIconMenu.SuspendLayout();
             SuspendLayout();
+            // 
+            // TrayIcon
+            // 
+            TrayIcon.Text = "notifyIcon1";
+            TrayIcon.Visible = true;
+            // 
+            // TrayIconMenu
+            // 
+            TrayIconMenu.ImageScalingSize = new Size(24, 24);
+            TrayIconMenu.Items.AddRange(new ToolStripItem[] { opToolStripMenuItem, webToolStripMenuItem, toolStripSeparator1, helpToolStripMenuItem, aboutToolStripMenuItem, toolStripSeparator2, exitToolStripMenuItem });
+            TrayIconMenu.Name = "TrayIconMenu";
+            TrayIconMenu.Size = new Size(241, 209);
+            // 
+            // opToolStripMenuItem
+            // 
+            opToolStripMenuItem.Name = "opToolStripMenuItem";
+            opToolStripMenuItem.Size = new Size(240, 32);
+            opToolStripMenuItem.Text = "Options";
+            // 
+            // webToolStripMenuItem
+            // 
+            webToolStripMenuItem.Name = "webToolStripMenuItem";
+            webToolStripMenuItem.Size = new Size(240, 32);
+            webToolStripMenuItem.Text = "ifconfig.co";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(237, 6);
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(240, 32);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(240, 32);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(237, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(240, 32);
+            exitToolStripMenuItem.Text = "Exit";
             // 
             // MainForm
             // 
@@ -42,9 +105,20 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Public IP Info Tool Setup";
             Load += MainForm_Load;
+            TrayIconMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private NotifyIcon TrayIcon;
+        private ContextMenuStrip TrayIconMenu;
+        private ToolStripMenuItem opToolStripMenuItem;
+        private ToolStripMenuItem webToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem exitToolStripMenuItem;
     }
 }
