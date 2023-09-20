e7985676f26a26b906f61eb9623c0c820a051bb1
twwct <twwc.tech@gmail.com>
2023-09-19 20:55:25 -0600

    new form backend connected

---
+++ Frontend/InfoWindow.cs
@@ -0,0 +1,15 @@
+﻿namespace PIPIT.Frontend
+{
+    public partial class InfoWindow : Form
+    {
+        public InfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
