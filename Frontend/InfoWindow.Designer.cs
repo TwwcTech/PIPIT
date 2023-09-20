d684779c7eef80d465cbc7d103a723b99370c8a8
twwct <twwc.tech@gmail.com>
2023-09-19 20:55:16 -0600

    new form added

---
+++ Frontend/InfoWindow.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace PIPIT.Frontend
+{
+    partial class InfoWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoWindow));
+            SuspendLayout();
+            // 
+            // InfoWindow
+            // 
+            AutoScaleMode = AutoScaleMode.Inherit;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "InfoWindow";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "InfoWindow";
+            Load += InfoWindow_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
