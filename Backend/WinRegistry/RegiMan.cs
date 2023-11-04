c4f0cdbe21885c76fd5817ba46d41a8e41a32cb6
Timothy Collins <twwc.tech@gmail.com>
2023-11-03 22:26:24 -0600

    updating static class to standard class

--- Backend/WinRegistry/RegiMan.cs
+++ Backend/WinRegistry/RegiMan.cs
@@ -1,30 +1,56 @@
 ﻿using Microsoft.Win32;
-using PIPIT.AppResources;
+using System.Reflection;
 
 namespace PIPIT.Backend.WinRegistry
 {
     internal class RegiMan
     {
-        public static void RegisterApp()
+        public string? AppName { get; } = "PIPIT";
+        public string? RegistryAppPath { get; set; }
+        public string? RegistryStartupPath { get; set; }
+        public static string? ShortcutPath { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "PIPIT.lnk");
+
+        public void RegisterApp()
         {
-            using (RegistryKey regKey = Registry.CurrentUser.CreateSubKey($"Software\\{StaticResources.AppName}"))
+            using RegistryKey regKey = Registry.CurrentUser.CreateSubKey($"{RegistryAppPath}\\{AppName}");
+            try
+            {
+                regKey.SetValue("InitDate", DateTime.Now.ToString("ddMMyyyy"));
+                regKey.SetValue("AppVersion", 1.0);
+            }
+            catch (Exception ex) { ex.ToString(); }
+        }
+
+        public bool IsAppRegistered()
+        {
+            using RegistryKey regStatusKey = Registry.CurrentUser.OpenSubKey($"Software\\{AppName}")!;
+            return regStatusKey != null;
+        }
+
+        public void AddToStartup()
+        {
+            using RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true)!; // Use "SOFTWARE" if having errors
+            if (regKey != null)
             {
                 try
                 {
-                    regKey.SetValue("InitDate", DateTime.Now.ToString("ddMMyyyy"));
-                    regKey.SetValue("AppVersion", StaticResources.Version);
+                    if (File.Exists(ShortcutPath))
+                    {
+                        regKey.SetValue(AppName, ShortcutPath);
+                    }
                 }
                 catch (Exception ex) { ex.ToString(); }
             }
         }
 
-        public static bool IsAppRegistered()
+        public bool IsStartupEnabled()
         {
-            using (RegistryKey regStatusKey = Registry.CurrentUser.OpenSubKey($"Software\\{StaticResources.AppName}")!)
+            using RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", false)!;
+            if (regKey != null)
             {
                 try
                 {
-                    if (regStatusKey != null)
+                    if (regKey.GetValue(AppName) != null)
                     {
                         return true;
                     }
@@ -34,55 +60,16 @@ namespace PIPIT.Backend.WinRegistry
             return false;
         }
 
-        public static void AddToStartup()
+        public void RemoveFromStartup()
         {
-            using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true)!) // Use "SOFTWARE" if having errors
+            using RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true)!;
+            if (regKey != null)
             {
-                if (regKey != null)
+                try
                 {
-                    try
-                    {
-                        if (File.Exists(StaticResources.ShortcutPath))
-                        {
-                            regKey.SetValue(StaticResources.AppName, StaticResources.ShortcutPath);
-                        }
-                    }
-                    catch (Exception ex) { ex.ToString(); }
+                    regKey.DeleteSubKey(AppName!, false);
                 }
-            }
-        }
-
-        public static bool IsStartupEnabled()
-        {
-            using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", false)!)
-            {
-                if (regKey != null)
-                {
-                    try
-                    {
-                        if (regKey.GetValue(StaticResources.AppName) != null)
-                        {
-                            return true;
-                        }
-                    }
-                    catch (Exception ex) { ex.ToString(); }
-                }
-            }
-            return false;
-        }
-
-        public static void RemoveFromStartup()
-        {
-            using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true)!)
-            {
-                if (regKey != null)
-                {
-                    try
-                    {
-                        regKey.DeleteSubKey(StaticResources.AppName, false);
-                    }
-                    catch (Exception ex) { ex.ToString(); }
-                }
+                catch (Exception ex) { ex.ToString(); }
             }
         }
     }
