6c533b30c4c732844f445c659c78244e156dc4a4
Timothy Collins <twwc.tech@gmail.com>
2023-11-03 23:17:21 -0600

    Shortcutter static class and method added

--- Backend/Shortcutter.cs
+++ Backend/Shortcutter.cs
@@ -1,19 +1,33 @@
-﻿using PIPIT.Backend.WinRegistry;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace PIPIT.Backend
 {
-    internal class Shortcutter
+    internal static class Shortcutter
     {
         public static void CreateShortcut()
         {
-            using StreamWriter shortcutWriter = new(RegiMan.ShortcutPath!);
+            string startupFolderPath = $@"C:\Users\{Environment.UserName}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup";
+            string shortcutFileName = "PIPIT.lnk";
+            string targetFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, shortcutFileName);
+            string shortcutPath = Path.Combine(startupFolderPath, shortcutFileName);
+
+            using StreamWriter shortcutWriter = new(shortcutPath!);
             try
             {
+                /* OLD */
+                //shortcutWriter.WriteLine("[Desktop Entry]");
+                //shortcutWriter.WriteLine($"Version={1.0}");
+                //shortcutWriter.WriteLine($"Name={Path.GetFileNameWithoutExtension(shortcutPath)}");
+                //shortcutWriter.WriteLine($"Exec={Assembly.GetExecutingAssembly().Location}");
+                //shortcutWriter.WriteLine("Terminal=false");
+                //shortcutWriter.WriteLine("Type=Application");
+                //shortcutWriter.Flush();
+
+                /* NEW */
                 shortcutWriter.WriteLine("[Desktop Entry]");
                 shortcutWriter.WriteLine($"Version={1.0}");
-                shortcutWriter.WriteLine($"Name={Path.GetFileNameWithoutExtension(RegiMan.ShortcutPath)}");
-                shortcutWriter.WriteLine($"Exec={Assembly.GetExecutingAssembly().Location}");
+                shortcutWriter.WriteLine($"Name={Path.GetFileNameWithoutExtension(shortcutFileName)}");
+                shortcutWriter.WriteLine($"Exec={targetFilePath}");
                 shortcutWriter.WriteLine("Terminal=false");
                 shortcutWriter.WriteLine("Type=Application");
                 shortcutWriter.Flush();
