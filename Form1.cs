2e68f77f953b52cce6b380cd897b54e82b89608b
Timothy Collins <twwc.tech@gmail.com>
2023-11-03 23:17:30 -0600

    comments added

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,3 @@
-using PIPIT.AppResources;
 using PIPIT.Backend;
 using PIPIT.Backend.WinRegistry;
 using PIPIT.Frontend;
@@ -15,57 +14,57 @@ namespace PIPIT
         private void MainForm_Load(object sender, EventArgs e)
         {
             // If this is not a new device
-            if (RegiMan.IsAppRegistered())
-            {
-                // If startup is enabled
-                if (RegiMan.IsStartupEnabled())
-                {
-                    // Start the tray icon and close the main window
-                    EnableStartupCheckbox.Checked = true;
-                    Close();
-                }
-                // Only start the tray icon
-                TrayIcon.Visible = true;
-            }
-            // If this is a new device
-            else
-            {
-                // Warning box showing on first time run
-                DialogResult dialogResult = MessageBox.Show("New device detected, continue?", "New Device Setup", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                switch (dialogResult)
-                {
-                    case DialogResult.Yes:
-                        RegiMan.RegisterApp();
-                        TrayIcon.Visible = true;
-                        break;
-                    case DialogResult.No:
-                        Dispose();
-                        break;
-                }
-            }
+        //    if (RegiMan.IsAppRegistered())
+        //    {
+        //        // If startup is enabled
+        //        if (RegiMan.IsStartupEnabled())
+        //        {
+        //            // Start the tray icon and close the main window
+        //            EnableStartupCheckbox.Checked = true;
+        //            Close();
+        //        }
+        //        // Only start the tray icon
+        //        TrayIcon.Visible = true;
+        //    }
+        //    // If this is a new device
+        //    else
+        //    {
+        //        // Warning box showing on first time run
+        //        DialogResult dialogResult = MessageBox.Show("New device detected, continue?", "New Device Setup", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        //        switch (dialogResult)
+        //        {
+        //            case DialogResult.Yes:
+        //                RegiMan.RegisterApp();
+        //                TrayIcon.Visible = true;
+        //                break;
+        //            case DialogResult.No:
+        //                Dispose();
+        //                break;
+        //        }
+        //    }
         }
 
         private void EnableStartupCheckbox_CheckedChanged(object sender, EventArgs e)
         {
-            // If checked is true add app to startup
-            if (EnableStartupCheckbox.Checked)
-            {
-                // If shortcut does not exist
-                if (!File.Exists(RegiMan.ShortcutPath))
-                {
-                    // Create Shortcut
-                    Shortcutter.CreateShortcut();
-                }
-                // Add app to startup folder via registry
-                RegiMan.AddToStartup();
-            }
-            else if (!EnableStartupCheckbox.Checked)
-            {
-                if (RegiMan.IsStartupEnabled())
-                {
-                    RegiMan.RemoveFromStartup();
-                }
-            }
+            //// If checked is true add app to startup
+            //if (EnableStartupCheckbox.Checked)
+            //{
+            //    // If shortcut does not exist
+            //    if (!File.Exists(RegiMan.ShortcutPath))
+            //    {
+            //        // Create Shortcut
+            //        Shortcutter.CreateShortcut();
+            //    }
+            //    // Add app to startup folder via registry
+            //    RegiMan.AddToStartup();
+            //}
+            //else if (!EnableStartupCheckbox.Checked)
+            //{
+            //    if (RegiMan.IsStartupEnabled())
+            //    {
+            //        RegiMan.RemoveFromStartup();
+            //    }
+            //}
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
